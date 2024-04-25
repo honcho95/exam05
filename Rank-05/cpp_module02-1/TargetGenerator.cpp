0672e3343b03939842c2f14d3c9394fc9c010212
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:50:39 +0200

    Update TargetGenerator.cpp

--- Rank-05/cpp_module02-1/TargetGenerator.cpp
+++ Rank-05/cpp_module02-1/TargetGenerator.cpp
@@ -4,18 +4,23 @@ TargetGenerator::TargetGenerator() {}
 
 TargetGenerator::~TargetGenerator() {}
 
-void TargetGenerator::learnTargetType(ATarget *target)
+void TargetGenerator::learnTargetType
+(ATarget *target)
 {
     if(target)
         map[target->getType()] = target;
 }
 
-void TargetGenerator::forgetTargetType(std::string const & type){
+void TargetGenerator::forgetTargetType
+(std::string const & type)
+{
     if(map.find(type) != map.end())
         map.erase(map.find(type));
 }
 
-ATarget *TargetGenerator::createTarget(std::string const &type){
+ATarget *TargetGenerator::createTarget
+(std::string const &type)
+{
     ATarget *pointeur = NULL;
     if(map.find(type) != map.end())
         pointeur = map[type];
