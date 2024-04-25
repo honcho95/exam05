7f4b510361a72c965f5218d045f3697a58d84e0d
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:47:53 +0200

    Update Polymorph.cpp

--- Rank-05/cpp_module02-1/Polymorph.cpp
+++ Rank-05/cpp_module02-1/Polymorph.cpp
@@ -1,9 +1,11 @@
 #include "Polymorph.hpp"
 
-Polymorph::Polymorph() : ASpell("Polymorph", "turned into a critter") {}
+Polymorph::Polymorph() : 
+ASpell("Polymorph", "turned into a critter") {}
 
 Polymorph::~Polymorph(){}
 
-ASpell *Polymorph::clone() const{
+ASpell *Polymorph::clone() const
+{
     return (new Polymorph());
 }
