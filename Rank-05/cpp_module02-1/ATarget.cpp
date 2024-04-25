9641cfc144002a89f35e16ce16b20a3664464e5b
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:46:24 +0200

    Update ATarget.cpp

--- Rank-05/cpp_module02-1/ATarget.cpp
+++ Rank-05/cpp_module02-1/ATarget.cpp
@@ -2,23 +2,32 @@
 
 ATarget::~ATarget() {}
 
-ATarget::ATarget(std::string const & type){
+ATarget::ATarget(std::string const & type)
+{
     this->_type = type;
 }
 
-ATarget::ATarget(ATarget const &obj){
+ATarget::ATarget(ATarget const &obj)
+{
     *this = obj;
 }
 
-ATarget &ATarget::operator=(ATarget const & obj){
+ATarget &ATarget::operator=(ATarget 
+const & obj)
+{
     this->_type = obj._type;
     return(*this);
 }
 
-const std::string &ATarget::getType() const{
+const std::string &ATarget::getType() const
+{
     return(this->_type);
 }
 
-void ATarget::getHitBySpell(ASpell const &spell) const{
-    std::cout << this->getType() << " has been " << spell.getEffects() << "!" << std::endl;
+void ATarget::getHitBySpell(ASpell 
+const &spell) const
+{
+    std::cout << this->getType() << 
+        " has been " << spell.getEffects() << 
+        "!" << std::endl;
 }
