0d001114f1ac3802a2c703944b11d1c02cbcaf06
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:37:37 +0200

    Update Warlock.cpp

--- Rank-05/cpp_module00/Warlock.cpp
+++ Rank-05/cpp_module00/Warlock.cpp
@@ -2,14 +2,17 @@
 
 Warlock::~Warlock()
 {
-    std::cout << this->getName() << ": My job here is done!" << std::endl;
+    std::cout << this->getName() <<
+        ": My job here is done!" << std::endl;
 }
 
-Warlock::Warlock(std::string const &name, std::string const &title)
+Warlock::Warlock(std::string const &name, 
+std::string const &title)
 {
     this->_name = name;
     this->_title = title;
-    std::cout << this->getName() << ": This looks like another boring day." << std::endl;
+    std::cout << this->getName() << ": This 
+    looks like another boring day." << std::endl;
 }
 
 const std::string& Warlock::getName() const
@@ -22,12 +25,15 @@ const std::string& Warlock::getTitle() const
     return (this->_title);
 }
 
-void Warlock::setTitle(std::string const &title)
+void Warlock::setTitle(std::string 
+const &title)
 {
     this->_title = title;
 }
 
 void Warlock::introduce() const
 {
-    std::cout << this->getName() << ": I am " << this->getName() << ", " << this->getTitle() << "!" << std::endl;
+    std::cout << this->getName() << ": I am " 
+        << this->getName() << ", " << 
+        this->getTitle() << "!" << std::endl;
 }
