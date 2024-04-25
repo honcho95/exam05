20c4efbcc95d835069e3e6380e9d8ad1281f2b95
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:41:03 +0200

    Update ATarget.hpp

--- Rank-05/cpp_module01/ATarget.hpp
+++ Rank-05/cpp_module01/ATarget.hpp
@@ -15,9 +15,11 @@ class ATarget
         virtual ATarget *clone() const = 0;
 
         ATarget(ATarget const &other);
-        ATarget &operator=(ATarget const &other);
+        ATarget &operator=(ATarget 
+const &other);
 
-        void getHitBySpell(ASpell const &spell) const;
+        void getHitBySpell(ASpell 
+const &spell) const;
 
     private :
         std::string _type;
