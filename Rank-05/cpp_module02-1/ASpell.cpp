e27e493b200ed8908baaa0b63088d4095388a735
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:45:09 +0200

    Update ASpell.cpp

--- Rank-05/cpp_module02-1/ASpell.cpp
+++ Rank-05/cpp_module02-1/ASpell.cpp
@@ -2,29 +2,38 @@
 
 ASpell::~ASpell() {}
 
-ASpell::ASpell(std::string const & name, std::string const & effects){
+ASpell::ASpell(std::string const & name, 
+std::string const & effects)
+{
     this->_name = name;
     this->_effects = effects;
 }
 
-ASpell::ASpell(ASpell const &obj){
+ASpell::ASpell(ASpell const &obj)
+{
     *this = obj;
 }
 
-ASpell &ASpell::operator=(ASpell const & obj){
+ASpell &ASpell::operator=(ASpell 
+const & obj)
+{
     this->_name = obj._name;
     this->_effects = obj._effects;
     return(*this);
 }
 
-const std::string &ASpell::getName() const{
+const std::string &ASpell::getName() const
+{
     return(this->_name);
 }
 
-const std::string ASpell::getEffects() const{
+const std::string ASpell::getEffects() const
+{
     return(this->_effects);
 }
 
-void ASpell::launch(ATarget const & target) const{
+void ASpell::launch(ATarget const & target) 
+const
+{
     target.getHitBySpell(*this);
 }
