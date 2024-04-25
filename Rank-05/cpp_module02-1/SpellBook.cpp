1fc026e684a37a91e774a0df80153ba3d0cd5599
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:49:26 +0200

    Update SpellBook.cpp

--- Rank-05/cpp_module02-1/SpellBook.cpp
+++ Rank-05/cpp_module02-1/SpellBook.cpp
@@ -9,26 +9,30 @@ void SpellBook::learnSpell(ASpell* spell){
         _spellbook.push_back(spell);
 }
 
-void SpellBook::forgetSpell(std::string const &spell){
+void SpellBook::forgetSpell(std::string 
+const &spell)
+{
     if(spell.empty())
         return;
     size_t i = 0;
     while(i < _spellbook.size())
     {
-        if(spell == _spellbook[i]->getName())
-            _spellbook.erase(_spellbook.begin() + i);
+        if(spell==_spellbook[i]->getName())
+            _spellbook.erase
+                (_spellbook.begin() + i);
         i++;
     }
 }
 
-ASpell* SpellBook::createSpell(std::string const &spell)
+ASpell* SpellBook::createSpell(std::string 
+const &spell)
 {
     ASpell *pointeur = NULL;
     size_t i = 0;
     
     while(i < _spellbook.size())
     {
-        if(spell == _spellbook[i]->getName())
+        if(spell==_spellbook[i]->getName())
             pointeur = _spellbook[i];
         i++;
     }
