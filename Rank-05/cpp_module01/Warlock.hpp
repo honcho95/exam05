7e3fcb96958dd0fa9b17e6af5f53b2214b165df1
honcho95 <92316807+honcho95@users.noreply.github.com>
2024-04-25 14:44:05 +0200

    Update Warlock.hpp

--- Rank-05/cpp_module01/Warlock.hpp
+++ Rank-05/cpp_module01/Warlock.hpp
@@ -10,19 +10,24 @@ class Warlock
 {
     public :
         ~Warlock();
-        Warlock(std::string const &name, std::string const &title);
+        Warlock(std::string const &name, 
+std::string const &title);
         const std::string& getName() const;
         const std::string& getTitle() const;
-        void setTitle(std::string const &title);
+        void setTitle
+(std::string const &title);
         void introduce() const;
 
         void learnSpell(ASpell *spell);
-        void forgetSpell(std::string spellName);
-        void launchSpell(std::string spellName, ATarget const &target);
+        void forgetSpell
+(std::string spellName);
+        void launchSpell
+(std::string spellName, ATarget const &target);
 
     private :
         Warlock(Warlock const &other);
-        Warlock &operator=(Warlock const &other);
+        Warlock &operator=
+(Warlock const &other);
         std::string _name;
         std::string _title;
         std::vector<ASpell*> vecSpell;
